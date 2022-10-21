6ab26990c41a788c726254daf5646798c2ede490
renato3x <pereirarenato21@gmail.com>
2022-10-21 09:06:08 -0300

    adicionando produtos na tela

--- diretivas/src/app/app.component.ts
+++ diretivas/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Produto } from './interfaces/Produto';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'diretivas';
+  nome: string = ''
+  imagem: string = ''
+  preco: number = 0
+  estoque: number = 0
+  descricao: string = ''
+
+  produtos: Produto[] = []
+
+  adicionarProduto(evento: any): void {
+    console.log(evento)
+    /**
+     * preventDefault() faz com que
+     * o comportamento padrão de um elemento
+     * HTML seja anulado
+     */
+    evento.preventDefault()
+
+    /**
+     * stopPropagation evita com que a emissão
+     * do evento afete outros elementos HTML
+     */
+    evento.stopPropagation()
+
+    this.produtos.push({
+      descricao: this.descricao,
+      estoque: this.estoque,
+      imagem: this.imagem,
+      nome: this.nome,
+      preco: this.preco
+    })
+  }
 }
